8e5dac9f74e7dd6352be8f29c3ca3c0db3307b22
Amir Zolfaghari <cvemys@gmail.com>
2023-02-05 02:50:51 +0330

    update AC3 benchmark

--- AC3_effect.py
+++ AC3_effect.py
@@ -3,17 +3,19 @@ from AC3 import AC_3
 from sys import stdin
 from datetime import timedelta, datetime
 from copy import deepcopy
+from BackTrack import BackTrackingSearch
 
 csp = csp_from_io(stdin)
+bt = BackTrackingSearch()
 start_time = datetime.now()
 csp_copy = deepcopy(csp)
-# backtracking(csp_copy)
+bt.search(csp_copy,{})
 runtime = datetime.now() - start_time
 print("Runtime Without Using AC-3: " + str(runtime))
 
 start_time = datetime.now()
 ac3 = AC_3()
 ac3(csp)
-# backtracking(csp)
+bt.search(csp,{})
 runtime = datetime.now() - start_time
 print("Runtime When Using AC-3: " + str(runtime))
