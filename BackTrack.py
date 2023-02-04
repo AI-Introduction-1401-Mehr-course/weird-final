f797e3ebaf3315fd2b9d2e02b0198cf1bcf36ded
Amir Zolfaghari <cvemys@gmail.com>
2023-02-05 02:41:02 +0330

    implement phase2

---
+++ BackTrack.py
@@ -0,0 +1,62 @@
+import math
+from WeirdFinalCSP import WeirdFinalCSP
+from safe_typing import Dict
+from copy import deepcopy
+
+class BackTrackingSearch:
+    
+    def search(self, csp: WeirdFinalCSP,assignments: Dict):
+        if self.is_complete(csp,assignments):
+            return assignments
+        var = self.MRV(csp,assignments)
+        if var==None:
+            return None
+        for val,_ in self.LCV(csp,var):
+            assignments[var] = val
+            fwd_csp = self.forwardChecking(csp,var,val)
+            result = self.search(fwd_csp,assignments)
+            if result!=None:
+                return result
+            del assignments[var]
+
+        return None
+
+    def is_complete(self,csp: WeirdFinalCSP,assignments:Dict):
+        return len(assignments) == csp.hall_count
+
+    def MRV(self, csp: WeirdFinalCSP, assignments: Dict):
+        mrv = None
+        min_val = math.inf
+        for var in range(csp.hall_count):
+            if var in assignments:
+                continue
+            val_count = len(csp.hall_group_candidates[var])
+            if val_count < min_val:
+                min_val = val_count
+                mrv = var
+
+        return mrv
+
+    def LCV(self, csp: WeirdFinalCSP, variable: int):
+        lcv = []
+        for val in csp.hall_group_candidates[variable]:
+            constraints = 0
+            for var in csp.hall_hall_exit_restrictions[variable]:
+                if val in csp.hall_group_candidates[var]:
+                    constraints += 1
+            lcv.append((val,constraints))
+
+        return sorted(lcv, key=lambda x:x[1])
+
+    def forwardChecking(self, csp: WeirdFinalCSP, variable: int, value: int):
+        new_csp = deepcopy(csp)
+        for var in csp.hall_hall_exit_restrictions[variable]:
+            new_group_candidate = []
+            for val in csp.hall_group_candidates[var]:
+                if val!=value:
+                    new_group_candidate.append(val)
+            mutable_group_candidates = list(new_csp.hall_group_candidates)
+            mutable_group_candidates[var] = new_group_candidate
+            new_csp.hall_group_candidates = tuple(mutable_group_candidates)
+
+        return new_csp
---
+++ main.py
@@ -0,0 +1,30 @@
+from WeirdFinalCSP import WeirdFinalCSP
+from serializer import csp_from_io
+import sys
+from BackTrack import BackTrackingSearch
+csp: WeirdFinalCSP
+
+
+def print_usage():
+    print(" usage:")
+    print("\tpython3", sys.argv[0], "path/to/problem_input.txt")
+
+
+if len(sys.argv) != 2:
+    print_usage()
+    sys.exit(1)
+
+with open(sys.argv[1], "r") as f:
+    csp = csp_from_io(f)
+
+    bt = BackTrackingSearch()
+
+    result = bt.search(csp,{})
+    if(result!=None):
+        for i in range(csp.hall_count):
+            print(result[i]+1,end=' ')
+    else:
+        print("No")
+
+
+
